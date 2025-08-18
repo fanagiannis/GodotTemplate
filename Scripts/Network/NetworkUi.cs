6919f4b716ed48b354399ba0dc60b3578c7e987b
fanagiannis <foulis67@gmail.com>
2025-08-18 10:46:03 +0300

    Tried Multiplayer

---
+++ Scripts/Network/NetworkUi.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class NetworkUi : Control
+{
+	public void OnServerPressed()
+	{
+		//NetworkHandler.StartServer();
+	}
+
+	public void OnClientPressed()
+	{
+
+	}
+}
